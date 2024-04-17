1062e36f51faeb811a0ce9b224e6e3fb0a9575d3
bsh <80870896+srisaffron@users.noreply.github.com>
2024-04-17 15:47:53 -0500

    create_vm.py

--- create_vm.py
+++ create_vm.py
@@ -3,18 +3,18 @@ from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.compute.models import HardwareProfile, NetworkProfile, OSProfile, StorageProfile, VirtualMachine, VirtualMachineProperties
 
 def create_vm(resource_group_name, vm_name, location, admin_username, admin_password):
-    subscription_id = 'YOUR_SUBSCRIPTION_ID'
+    subscription_id = '9718b645-776a-420e-b2a3-1e82dd1d1536'
     credentials = AzureCliCredential()
 
     compute_client = ComputeManagementClient(credentials, subscription_id)
 
     # Define VM parameters for Windows VM
     vm_parameters = {
-        'location': location,
+        'location': westus,
         'os_profile': {
-            'computer_name': vm_name,
-            'admin_username': admin_username,
-            'admin_password': admin_password
+            'computer_name': vm_py,
+            'admin_username': sriharshitha,
+            'admin_password': SriHarshitha@123
         },
         'hardware_profile': {
             'vm_size': 'Standard_B1s'
