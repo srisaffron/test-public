1dad9767473fbc80ed44a43f202b98a56aa6fa20
bsh <80870896+srisaffron@users.noreply.github.com>
2024-04-17 15:39:38 -0500

    delete_vm.py

--- delete_vm.py
+++ delete_vm.py
@@ -2,14 +2,14 @@ from azure.identity import AzureCliCredential
 from azure.mgmt.compute import ComputeManagementClient
 
 def delete_vm(resource_group_name, vm_name):
-    subscription_id = 9718b645-776a-420e-b2a3-1e82dd1d1536
+    subscription_id = 'YOUR_SUBSCRIPTION_ID'
     credentials = AzureCliCredential()
 
-    compute_client = ComputeManagementClient(credentials, 9718b645-776a-420e-b2a3-1e82dd1d1536)
+    compute_client = ComputeManagementClient(credentials, subscription_id)
 
     # Delete VM
-    async_vm_deletion = compute_client.virtual_machines.begin_delete(harshi, vm_py)
+    async_vm_deletion = compute_client.virtual_machines.begin_delete(resource_group_name, vm_name)
     async_vm_deletion.wait()
 
 # Usage example
-delete_vm('harshi', 'my_py')
+delete_vm('myResourceGroup', 'myWindowsVM')
